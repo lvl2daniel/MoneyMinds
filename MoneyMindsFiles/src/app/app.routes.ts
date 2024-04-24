6fcd8b248ab540cb42066a54bcfcda54868b070e
Jason Griller <ja194255@ucf.edu>
2024-04-24 06:40:09 -0400

    Final changes

--- MoneyMindsFiles/src/app/app.routes.ts
+++ MoneyMindsFiles/src/app/app.routes.ts
@@ -4,10 +4,8 @@ import { HomeComponent } from './home/home.component';
 import { Chapter1Component } from './chapter1/chapter1.component';
 import { GameComponent } from './game/game.component';
 import { QuizComponent } from './quiz/quiz.component';
-import { PricingComponent } from './pricing/pricing.component';
 
 export const routes: Routes = [
-    { path: 'pricing', component: PricingComponent},
     { path: 'dashboard', component: DashComponent},
     { path: 'home', component: HomeComponent},
     { path: 'game', component: GameComponent},
